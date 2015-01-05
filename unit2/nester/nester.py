7a8f858adee8d4f25445074101c333f53b9b675a
skandpurohit <skandpurohit@gmail.com>
2015-01-04 23:02:53 -0700

    day2 commit

--- unit2/nester/nester.py
+++
@@ -1,9 +0,0 @@
-"""This function prints the array and sub-arrays"""
-def recursiveArray(item):
-	"""It takes an argument in form of list"""
-	for each_item in item:
-		if isinstance(each_item,list):
-			recursiveArray(each_item)
-		else:
-			print(each_item)
-			
